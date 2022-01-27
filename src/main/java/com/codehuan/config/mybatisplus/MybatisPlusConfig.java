d73080a3798e4cb841e8d1a56a8080b170541f26
ZhangHuan <1730272469@qq.com>
2022-01-27 17:06:00 +0800

    MybatisPlusConfig

---
+++ src/main/java/com/codehuan/config/mybatisplus/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.codehuan.config.mybatisplus;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ZhangHuan created on 2022/1/27 15:45
+ * @version 1.0
+ * @Description TODO
+ * @email: 1730272469@qq.com
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
